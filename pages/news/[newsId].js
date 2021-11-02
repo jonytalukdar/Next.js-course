f729e0e8166d52550c5a9a825edabbd930f15714
jonytalukdar <jonytalukdar30@gmail.com>
2021-11-02 11:55:49 +0600

    extracting dynamic values

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,13 @@
-import React from 'react';
+import { useRouter } from 'next/router';
 
 const DetailPage = () => {
+  const router = useRouter();
+
+  const newsId = router.query.newsId;
+
+  //send a request to the backend api
+  // fetching newsDetail with the help of newsId
+
   return <h1>The Detail page</h1>;
 };
 
