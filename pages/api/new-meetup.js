03324ebe74e0852da62d1986cb32dc089bdccded
jonytalukdar <jonytalukdar30@gmail.com>
2021-11-02 15:45:00 +0600

    added mongodb

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,7 +1,19 @@
-const handler = (req, res) => {
+import { MongoClient } from 'mongodb';
+
+const handler = async (req, res) => {
   if (req.method === 'POST') {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      'mongodb+srv://meetupUser:ZnocMwIbS6Q2IL6E@cluster0.shsop.mongodb.net/meetups?retryWrites=true&w=majority'
+    );
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+    const result = meetupsCollection.insertOne(data);
+    console.log(result);
   }
 };
+
+export default handler;
