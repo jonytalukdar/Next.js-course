f86806dbf4c0235c448f9665413480757bc9ec6d
jonytalukdar <jonytalukdar30@gmail.com>
2021-11-02 12:07:02 +0600

    linking page

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,19 @@
+import Link from 'next/link';
+
 const NewsPage = () => {
-  return <h1>The News Page</h1>;
+  return (
+    <>
+      <h1>The News Page</h1>;
+      <ul>
+        <li>
+          <Link href="/news/id">News Details</Link>
+        </li>
+        <li>
+          <a href="">Something else</a>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default NewsPage;
