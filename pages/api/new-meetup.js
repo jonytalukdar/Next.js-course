506e734c1bcd9f08d95e13b2badb13e25b860e00
jonytalukdar <jonytalukdar30@gmail.com>
2021-11-02 16:04:34 +0600

    added http request to mongodb

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,19 +1,27 @@
 import { MongoClient } from 'mongodb';
 
-const handler = async (req, res) => {
+// /api/new-meetup
+// POST /api/new-meetup
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
 
     const client = await MongoClient.connect(
       'mongodb+srv://meetupUser:ZnocMwIbS6Q2IL6E@cluster0.shsop.mongodb.net/meetups?retryWrites=true&w=majority'
     );
-
     const db = client.db();
 
     const meetupsCollection = db.collection('meetups');
-    const result = meetupsCollection.insertOne(data);
+
+    const result = await meetupsCollection.insertOne(data);
+
     console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
   }
-};
+}
 
 export default handler;
