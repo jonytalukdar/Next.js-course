a05a773d7a95973b4bc4e7e3b1e352541f52199e
jonytalukdar <jonytalukdar30@gmail.com>
2021-11-02 12:43:43 +0600

    showing meetups list

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,26 @@
-const index = () => {
-  return (
-    <div>
-      <h1>hello</h1>
-    </div>
-  );
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 5, 12345 Some City',
+    description: 'This is a first meetup!',
+  },
+  {
+    id: 'm2',
+    title: 'A Second Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 10, 12345 Some City',
+    description: 'This is a second meetup!',
+  },
+];
+
+const HomePage = () => {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 };
 
-export default index;
+export default HomePage;
