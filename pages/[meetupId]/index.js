8291c74477bea38d52544e822d57086d942d4b1f
jonytalukdar <jonytalukdar30@gmail.com>
2021-11-02 15:12:12 +0600

    working with getStaticPaths

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -17,6 +17,20 @@ const MeetupDetailPage = () => {
   );
 };
 
+export const getStaticPaths = async () => {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: { meetupId: 'm1' },
+      },
+      {
+        params: { meetupId: 'm2' },
+      },
+    ],
+  };
+};
+
 export const getStaticProps = async (context) => {
   //fetch data from api
   const meetupId = context.params.meetupId;
